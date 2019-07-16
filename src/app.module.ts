fb3d4fe766ff91bdedc872c3c8ae0d3ef5e2a908
kuqoi <kuqoi@qq.com>
2019-07-16 23:11:16 +0800

    init

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { MissionModule } from './mission/mission.module'
+
+@Module({
+  imports: [MissionModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
